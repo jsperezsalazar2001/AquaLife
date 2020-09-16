4f7993cc73622739466c11f6e5462f7712d442e7
Naitsabes <jsperezsalazar2001@gmail.com>
2020-09-15 20:37:31 -0500

    Adding home view

--- AquaLife/resources/views/home/index.blade.php
+++ AquaLife/resources/views/home/index.blade.php
@@ -5,22 +5,20 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                @include('util.message')
-                @if($errors->any())
-                    @foreach($errors->all() as $error)
-                        <div class="alert alert-danger alert-block">
-                            <button type="button" class="close" data-dismiss="alert">x</button>
-                            <strong>{{ $error }}</strong>
-                        </div>
-                    @endforeach
-                @endif
-                </div>
+        <div class="col-md-10 bg-transparent welcome-div">
+            <div class="col-12">
+                <img src="{{ asset('/logos/logo_03.png') }}" class="card-img-top">
+                <h1 class="display-4 welcome" align="center"><strong>{{ __('home.welcome') }}</strong></h1>
             </div>
+        @include('util.message')
+        @if($errors->any())
+            @foreach($errors->all() as $error)
+                <div class="alert alert-danger alert-block">
+                    <button type="button" class="close" data-dismiss="alert">x</button>
+                    <strong>{{ $error }}</strong>
+                </div>
+            @endforeach
+        @endif
         </div>
     </div>
 </div>
